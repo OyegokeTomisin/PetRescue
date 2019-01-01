9c72bc8cfbc9e8db2de62f7b32296235e257a09e
Oyegoke Tomisin <tomisin18@gmail.com>
2019-01-01 10:12:50 +0100

    Added Form Table and Custom Cells

--- PetRescue/AdoptionFormViewController.swift
+++ PetRescue/AdoptionFormViewController.swift
@@ -11,9 +11,65 @@ import UIKit
 class AdoptionFormViewController: UIViewController {
     
     var index = 0
+    var cell_types = ["embeddedPhoto","text","yesno", "formattednumeric", "datetime", "text"]
+
+    @IBOutlet weak var adoptionFormTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        configureTableView()
     }
+}
+
+extension AdoptionFormViewController: UITableViewDataSource, UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // try enum for selecting cell types
+        if cell_types[indexPath.row] == "text"{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "text") as! TextTableViewCell
+            
+            return cell
+        }
+        else if cell_types[indexPath.row] == "embeddedPhoto"{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "embeddedPhoto") as! EmbeddedPhotoTableViewCell
+
+            return cell
+        }
+        else if cell_types[indexPath.row] == "yesno"{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "yesno") as! YesNoTableViewCell
+            
+            return cell
+        }
+        else if cell_types[indexPath.row] == "formattednumeric"{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "formattednumeric") as! FormattedNumericTableViewCell
+            
+            return cell
+        }
+        else if cell_types[indexPath.row] == "datetime"{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "datetime") as! DateTimeTableViewCell
+            
+            return cell
+        }
+        else{
+            return UITableViewCell()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func configureTableView() {
+        
+        adoptionFormTable.delegate = self
+        adoptionFormTable.dataSource = self
+        
+        adoptionFormTable.register(UINib(nibName: "TextTableViewCell", bundle: nil), forCellReuseIdentifier: "text")
+        adoptionFormTable.register(UINib(nibName: "EmbeddedPhotoTableViewCell", bundle: nil), forCellReuseIdentifier: "embeddedPhoto")
+        adoptionFormTable.register(UINib(nibName: "YesNoTableViewCell", bundle: nil), forCellReuseIdentifier: "yesno")
+        adoptionFormTable.register(UINib(nibName: "FormattedNumericTableViewCell", bundle: nil), forCellReuseIdentifier: "formattednumeric")
+        adoptionFormTable.register(UINib(nibName: "DateTimeTableViewCell", bundle: nil), forCellReuseIdentifier: "datetime")
+    }
+    
 }
---
+++ PetRescue/Custom Cells/FormattedNumericTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FormattedNumericTableViewCell.swift
+//  PetRescue
+//
+//  Created by OYEGOKE TOMISIN on 01/01/2019.
+//  Copyright © 2019 com.OyegokeTomisin. All rights reserved.
+//
+
+import UIKit
+
+class FormattedNumericTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
